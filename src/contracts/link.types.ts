dcf5205a0b3c96d2bc0465e24770775706e5258a
odracirdev <ricardocuauro93@gmail.com>
2024-10-20 18:49:56 -0300

    feat: :sparkles: introducing RRSS section in header

---
+++ src/contracts/link.types.ts
@@ -0,0 +1,15 @@
+export interface SocialLink {
+  // Texto del enlace.
+  label: string
+
+  //  URL del enlace.
+  link: string
+
+  // Color del enlace.
+  color?: string
+}
+
+export type SocialIconLink = SocialLink & {
+  // Icono del enlace.
+  icon: string
+}
---
+++ src/data/social.data.ts
@@ -0,0 +1,34 @@
+import type { SocialIconLink } from '@contracts'
+
+export const socials: SocialIconLink[] = [
+  {
+    label: 'Twitch',
+    icon: 'twitch',
+    link: 'https://www.twitch.tv/odracirdev',
+    color: '#9146FF'
+  },
+  {
+    label: 'YouTube',
+    icon: 'youtube',
+    link: 'https://www.youtube.com/@ricardcuauro?sub_confirmation=1',
+    color: '#FF0000'
+  },
+  {
+    label: 'Discord',
+    icon: 'discord',
+    link: 'https://discord.gg/r6tFDfvYW6',
+    color: '#5865F2'
+  },
+  {
+    label: 'GitHub',
+    icon: 'github',
+    link: 'https://github.com/odracirdev',
+    color: 'var(--light)'
+  },
+  {
+    label: 'LinkedIn',
+    icon: 'linkedin',
+    link: 'https://www.linkedin.com/in/ricardocuauro/',
+    color: '#0A66C2'
+  }
+]
