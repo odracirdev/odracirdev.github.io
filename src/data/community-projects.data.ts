246791d7df9ddde13228f87eacbf17fb60de6a89
odracirdev <ricardocuauro93@gmail.com>
2024-10-23 02:12:36 -0300

    feat: :sparkles: add communities projects

---
+++ src/data/community-projects.data.ts
@@ -0,0 +1,23 @@
+export const communityProjects = [
+  {
+    name: 'JSConf Chile 2024',
+    community: 'JavaScript Chile',
+    tech: 'techs/astro',
+    icon: '/images/communities/jschile.webp',
+    link: 'https://jsconf.cl/'
+  },
+  {
+    name: 'UX Corp Rangel',
+    community: 'UX Corp Rangel',
+    tech: 'techs/astro',
+    icon: '/images/communities/uxcorp.webp',
+    link: 'https://uxcorprangel.github.io/'
+  },
+  {
+    name: 'Ana×Cards',
+    community: 'UX Corp Rangel',
+    tech: 'techs/astro',
+    icon: '/images/communities/uxcorp.webp',
+    link: 'https://uxcorprangel.github.io/ana-cards-web/'
+  }
+]
