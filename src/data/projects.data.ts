5aaf6aa3c8327b7f5369fbcc0724c1b205aecf76
odraciRdev <ricardocuauro93@gmail.com>
2025-07-23 11:22:36 -0400

    feat: add 'Store for Devs' project and update project display logic

--- src/data/projects.data.ts
+++ src/data/projects.data.ts
@@ -1,5 +1,11 @@
 export const projects = [
   {
+    image: '/images/projects/store-for-devs.webp',
+    name: 'Store for Devs',
+    desc: 'Una plantilla de astro para crear tiendas online con WooCommerce.',
+    link: 'https://github.com/odracirdev/store-for-devs'
+  },
+  {
     image: '/images/projects/deluxe-beauty.webp',
     name: 'Deluxe Beauty',
     desc: 'Una manera simple de agendar servicios de belleza a domicilio.',
